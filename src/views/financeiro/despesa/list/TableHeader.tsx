0c9233d6e216836d0e0e46e13e49a71f19c5d211
sigespweb22 <sigespweb22@gmail.com>
2023-02-03 14:13:39 -0300

    ajustes na logo da app on issue #20

--- src/views/financeiro/despesa/list/TableHeader.tsx
+++ src/views/financeiro/despesa/list/TableHeader.tsx
@@ -1,17 +1,17 @@
-import { useContext   } from 'react';
+import { useContext } from 'react'
 
 // ** MUI Imports
 import Box from '@mui/material/Box'
 import Button from '@mui/material/Button'
 import TextField from '@mui/material/TextField'
-import Tooltip from '@mui/material/Tooltip';
+import Tooltip from '@mui/material/Tooltip'
 
 // ** Import Toast
-import 'react-toastify/dist/ReactToastify.css';
+import 'react-toastify/dist/ReactToastify.css'
 
 // ** Context Imports
 import { AbilityContext } from 'src/layouts/components/acl/Can'
-import { Grid } from '@mui/material';
+import { Grid } from '@mui/material'
 
 interface TableHeaderProps {
   value: string
@@ -22,31 +22,34 @@ interface TableHeaderProps {
 const TableHeader = (props: TableHeaderProps) => {
   // ** Hook
   const ability = useContext(AbilityContext)
-  
+
   // ** Props
   const { handleFilter, toggle, value } = props
 
   return (
     <Grid xs={12} md={12} lg={12} sx={{ p: 5, position: 'relative' }}>
-  <TextField
-      size='small'
-      value={value}
-      sx={{ width: 'calc(100% - 80px)' }}
-      variant="outlined" 
-      placeholder={"Busca despesa pelo nome fantasia do cliente"}
-      onChange={e => handleFilter(e.target.value)}
-    />
-  {ability?.can('create', 'ac-despesa-page') ? (
-      <Tooltip title={"Adicionar nova despesa"}>
-        <Button sx={{ mr: 4, position: 'absolute', right: 0, top: '50%', transform: 'translateY(-50%)' }} onClick={toggle} variant='contained'>
-          Novo
-        </Button>
-      </Tooltip>
-    ) : <></>}
-</Grid>
-
-
-
+      <TextField
+        size='small'
+        value={value}
+        sx={{ width: 'calc(100% - 80px)' }}
+        variant='outlined'
+        placeholder={'Busca despesa pelo nome fantasia do cliente'}
+        onChange={e => handleFilter(e.target.value)}
+      />
+      {ability?.can('create', 'ac-despesa-page') ? (
+        <Tooltip title={'Adicionar nova despesa'}>
+          <Button
+            sx={{ mr: 4, position: 'absolute', right: 0, top: '50%', transform: 'translateY(-50%)' }}
+            onClick={toggle}
+            variant='contained'
+          >
+            Novo
+          </Button>
+        </Tooltip>
+      ) : (
+        <></>
+      )}
+    </Grid>
   )
 }
 
